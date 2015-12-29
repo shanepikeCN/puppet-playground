2c5c94e1669a0fc51c4a96f6fbff7d1d3f4fbab8
U-Shane-PC\Shane <shane.pike@outlook.com>
2015-12-29 08:15:27 +0800

    First commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,12 @@
 # This is the manifest used when provisiong Puppet on the Vagrant boxes.
 # Place here your Puppet test code or manage directly with the play command.
 
+
+file { 'motd':
+	path	=> 'etc/motd'
+	content => 'This is your message of the day' 
+}
+
+node 'test-server-1' {
+	include apache
+}
